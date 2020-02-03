726b27e0197e2f473010b0bf4aa6aa5333298ef9
liyahui <15738773643@163.com>
2020-02-03 18:20:16 +0800

    updtae loginout

--- src/com/sa/service/server/ServerLoginOut.java
+++ src/com/sa/service/server/ServerLoginOut.java
@@ -24,30 +24,6 @@ public class ServerLoginOut extends Packet{
 
 	@Override
 	public void execPacket() {
-		People people = null;
-		String[] roomIds = this.getRoomId().split(",");
-		if (null != roomIds && roomIds.length > 0) {
-			for (String rId : roomIds) {
-				/** 根据房间id 和 发信人id 查询人员信息 */
-				people = ServerDataPool.dataManager.getRoomUesr(rId, this.getFromUserId());
-				if(Objects.nonNull(people)){
-					break;
-				}
-			}
-		}
-
-		/** 如果人员信息为空 */
-		if (null != people)
-			/** 设置记录集选项 为 delete */
-			this.setOption(255, "deleted");
-
-		/** 如果有中心 */
-		if (ConfManager.getIsCenter()) {
-			/** 将消息转发到中心 */
-			Manager.INSTANCE.sendPacketToCenter(this, Constant.CONSOLE_CODE_TS);
-		}
-		/** 实例化消息回执并执行 */
-		// new ClientMsgReceipt(this.getPacketHead()).execPacket();
 		/** 实例化登出 下行 并执行 */
 		ClientLoginOut clientLoginOut = new ClientLoginOut(this.getPacketHead(), this.getOptions());
 		clientLoginOut.execPacket();
