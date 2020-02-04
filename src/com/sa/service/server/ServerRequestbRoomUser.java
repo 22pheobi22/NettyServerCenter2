cec7fd0f3d904d51a072039673b37ae1786d8ded
liyahui <15738773643@163.com>
2020-02-04 23:02:27 +0800

    update 獲取roomuser

--- src/com/sa/service/server/ServerRequestbRoomUser.java
+++ src/com/sa/service/server/ServerRequestbRoomUser.java
@@ -14,21 +14,12 @@
  */
 package com.sa.service.server;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import com.sa.base.ConfManager;
-import com.sa.base.Manager;
-import com.sa.base.ServerDataPool;
-import com.sa.base.element.People;
 import com.sa.net.Packet;
 import com.sa.net.PacketType;
-import com.sa.service.client.ClientResponebRoomUser;
-import com.sa.util.Constant;
 
 public class ServerRequestbRoomUser extends Packet {
-	public ServerRequestbRoomUser(){}
+	public ServerRequestbRoomUser() {
+	}
 
 	@Override
 	public PacketType getPacketType() {
@@ -38,76 +29,6 @@ public class ServerRequestbRoomUser extends Packet {
 	@Override
 	public void execPacket() {
 
-		/** 如果有中心 并 目标IP不是中心IP */
-		if (ConfManager.getIsCenter() && !ConfManager.getCenterIp().equals(this.getRemoteIp())) {
-			// this.setOption(1, json);
-			/** 转发给中心 */
-			Manager.INSTANCE.sendPacketToCenter(this, Constant.CONSOLE_CODE_TS);
-		} else {
-			String[] roomIds = this.getRoomId().split(",");
-			if (null != roomIds && roomIds.length > 0) {
-				for (String rId : roomIds) {
-					/** 根据房间id获取房间内用户信息 */
-					Map<String, People> hm = ServerDataPool.dataManager.getRoomUesrs(rId);
-
-					int index = 0;
-					String json = "[";
-					/** 遍历用户信息 */
-					for (Entry<String, People> people : hm.entrySet()) {
-						if (index % Constant.PEOPLE_NUM == 0)
-							json = "[";
-						index++;
-
-						json += toJson(people);
-
-						if ((index % Constant.PEOPLE_NUM == 0 || index == hm.size()) && json.length() > 1) {
-							json = json.substring(0, json.length() - 1);
-
-							json += "]";
-							/** 实例化获取房间列表 下行 并赋值 */
-							ClientResponebRoomUser crru = new ClientResponebRoomUser(this.getPacketHead());
-							/** json格式的用户信息放入 选项 1 中 */
-							crru.setOption(1, json);
-							crru.setRoomId(rId);
-							/** 执行 */
-							crru.execPacket();
-						}
-
-					}
-				}
-			}
-
-		}
-
-	}
-
-	private String toJson(Entry<String, People> people) {
-		StringBuffer sb = new StringBuffer();
-
-		sb.append("{\"id\":\"" + people.getKey() + "\",\"name\":\"" + people.getValue().getName() + "\",\"icon\":\""
-				+ people.getValue().getIcon() + "\",\"role\":[");
-		for (Iterator iter = people.getValue().getRole().iterator(); iter.hasNext();) {
-			sb.append("\"" + (String) iter.next()).append("\",");
-		}
-
-		if (0 < people.getValue().getRole().size()) {
-			sb.deleteCharAt(sb.length() - 1);
-		}
-
-		sb.append("],\"auth\":[");
-
-		for (Map.Entry<String, Integer> auth : people.getValue().getAuth().entrySet()) {
-			sb.append("{\"" + auth.getKey()).append("\":").append(auth.getValue()).append("},");
-		}
-
-		if (0 < people.getValue().getAuth().size()) {
-			sb.deleteCharAt(sb.length() - 1);
-		}
-
-		sb.append("],").append("\"agoraId\":\"").append(people.getValue().getAgoraId()).append("\"");
-		sb.append("},");
-
-		return sb.toString();
 	}
 
 }
