b7db8d5ed3156be7050c6ed72d55dfdd5865301b
liyahui <15738773643@163.com>
2020-02-04 13:39:52 +0800

    update 通知在线用户

--- src/com/sa/service/server/ServerRequestbAll.java
+++ src/com/sa/service/server/ServerRequestbAll.java
@@ -14,21 +14,13 @@
  */
 package com.sa.service.server;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import com.sa.base.ConfManager;
-import com.sa.base.Manager;
 import com.sa.net.Packet;
 import com.sa.net.PacketType;
-import com.sa.service.client.ClientMsgReceipt;
 import com.sa.service.client.ClientResponebAll;
-import com.sa.service.permission.Permission;
-import com.sa.util.Constant;
 
 public class ServerRequestbAll extends Packet {
-	public ServerRequestbAll(){}
+	public ServerRequestbAll() {
+	}
 
 	@Override
 	public PacketType getPacketType() {
@@ -37,23 +29,9 @@ public class ServerRequestbAll extends Packet {
 
 	@Override
 	public void execPacket() {
-		/** 校验用户角色是否符合*/
-		Set<String> checkRoleSet = new HashSet(){{add(Constant.ROLE_SYSTEM);}};
-		Map<String, Object> result = Permission.INSTANCE.checkUserRole(this.getRoomId(), this.getFromUserId(), checkRoleSet);
-		/** 如果校验成功*/
-		if (0 == ((Integer) result.get("code"))) {
-			/** 实例化 发送全体消息 下行 并执行*/
-			ClientResponebAll clientResponebAll = new ClientResponebAll(this.getPacketHead(), this.getOptions());
-			clientResponebAll.execPacket();
-			/** 如果有中心 并且 中心不是目标地址*/
-			if (ConfManager.getIsCenter() && !ConfManager.getCenterIp().equals(this.getRemoteIp())) {
-				/** 转发到中心*/
-				Manager.INSTANCE.sendPacketToCenter(clientResponebAll, Constant.CONSOLE_CODE_TS);
-			}
-		}
-		/** 实例化消息回执 赋值 并 执行*/
-		new ClientMsgReceipt(this.getPacketHead(), result).execPacket();
-
+		/** 实例化 发送全体消息 下行 并执行 */
+		ClientResponebAll clientResponebAll = new ClientResponebAll(this.getPacketHead(), this.getOptions());
+		clientResponebAll.execPacket();
 	}
 
 }
