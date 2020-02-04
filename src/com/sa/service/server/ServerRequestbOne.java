8389aa9929ee9ba0eca8ca563abadd6a24a1089d
liyahui <15738773643@163.com>
2020-02-04 15:40:53 +0800

    updte 通知指定用户

--- src/com/sa/service/server/ServerRequestbOne.java
+++ src/com/sa/service/server/ServerRequestbOne.java
@@ -14,21 +14,15 @@
  */
 package com.sa.service.server;
 
-import java.util.Map;
-
-import com.sa.base.ConfManager;
-import com.sa.base.Manager;
 import com.sa.base.ServerDataPool;
 import com.sa.base.element.People;
 import com.sa.net.Packet;
 import com.sa.net.PacketType;
-import com.sa.service.client.ClientMsgReceipt;
 import com.sa.service.client.ClientResponebOne;
-import com.sa.service.permission.Permission;
-import com.sa.util.Constant;
 
 public class ServerRequestbOne extends Packet {
-	public ServerRequestbOne(){}
+	public ServerRequestbOne() {
+	}
 
 	@Override
 	public PacketType getPacketType() {
@@ -37,33 +31,22 @@ public class ServerRequestbOne extends Packet {
 
 	@Override
 	public void execPacket() {
-		/** 校验用户权限*/
-		Map<String, Object> result = Permission.INSTANCE.checkUserAuth(this.getRoomId(), this.getFromUserId(), Constant.AUTH_SPEAK);
-		/** 如果校验成功*/
-		if (0 == ((Integer) result.get("code"))) {
-			String[] roomIds = this.getRoomId().split(",");
-			if(roomIds!=null&&roomIds.length>0){
-				for (String rId : roomIds) {
-					/** 根据房间id 和 目标用户id 获取 人员信息*/
-					People people = ServerDataPool.dataManager.getRoomUesr(rId, this.getToUserId());
-					/** 实例化一对一消息类型 下行 并 赋值*/
-					ClientResponebOne clientResponebOne = new ClientResponebOne(this.getPacketHead(), this.getOptions());
-					/** 如果人员信息不为空*/
-					if (null != people) {
-						//设置房间id为目标房间id
-						clientResponebOne.setRoomId(rId);
-						/** 执行 一对一消息发送 下行*/
-						clientResponebOne.execPacket();
-								/** 如果有中心 并 中心ip不是 目标ip*/
-					} else  if (ConfManager.getIsCenter() && !ConfManager.getCenterIp().equals(this.getRemoteIp())) {
-						/** 发送 下行类型 到中心*/
-						Manager.INSTANCE.sendPacketToCenter(clientResponebOne, Constant.CONSOLE_CODE_TS);
-						break;
-					}
+		String[] roomIds = this.getRoomId().split(",");
+		if (roomIds != null && roomIds.length > 0) {
+			for (String rId : roomIds) {
+				/** 根据房间id 和 目标用户id 获取 人员信息 */
+				People people = ServerDataPool.dataManager.getRoomUesr(rId, this.getToUserId());
+				/** 实例化一对一消息类型 下行 并 赋值 */
+				ClientResponebOne clientResponebOne = new ClientResponebOne(this.getPacketHead(),
+						this.getOptions());
+				/** 如果人员信息不为空 */
+				if (null != people) {
+					// 设置房间id为目标房间id
+					clientResponebOne.setRoomId(rId);
+					/** 执行 一对一消息发送 下行 */
+					clientResponebOne.execPacket();
 				}
 			}
 		}
-		/** 实例化 消息回执 并 赋值 并 执行*/
-		new ClientMsgReceipt(this.getPacketHead(), result).execPacket();
 	}
 }
