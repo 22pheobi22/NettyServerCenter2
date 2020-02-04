5630ffe08caad271c54d9f69870c5a72e22e0851
user.name <Administrator@DESKTOP-M55IN4K>
2020-02-04 17:54:32 +0800

    update 獲取roomchat

--- src/com/sa/service/client/ClientResponebRoomChat.java
+++ src/com/sa/service/client/ClientResponebRoomChat.java
@@ -1,10 +1,8 @@
 package com.sa.service.client;
 
-import com.sa.base.Manager;
 import com.sa.net.Packet;
 import com.sa.net.PacketHeadInfo;
 import com.sa.net.PacketType;
-import com.sa.util.Constant;
 
 public class ClientResponebRoomChat extends Packet {
 
@@ -23,12 +21,6 @@ public class ClientResponebRoomChat extends Packet {
 
 	@Override
 	public void execPacket() {
-		try {
-			this.setToUserId(getFromUserId());
-			Manager.INSTANCE.sendPacketTo(this, Constant.CONSOLE_CODE_S);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 	@Override
