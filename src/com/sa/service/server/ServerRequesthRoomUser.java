502be2a38d02ed9a8b7c89c806473f63d18c40b0
user.name <Administrator@DESKTOP-M55IN4K>
2020-02-05 09:16:34 +0800

    update http getroomuser

--- src/com/sa/service/server/ServerRequesthRoomUser.java
+++ src/com/sa/service/server/ServerRequesthRoomUser.java
@@ -1,16 +1,7 @@
 package com.sa.service.server;
 
-import java.util.Map;
-
-import com.alibaba.fastjson.JSONObject;
-import com.sa.base.ConfManager;
-import com.sa.base.Manager;
-import com.sa.base.ServerDataPool;
-import com.sa.base.element.People;
 import com.sa.net.Packet;
 import com.sa.net.PacketType;
-import com.sa.service.client.ClientResponehRoomUser;
-import com.sa.util.Constant;
 
 public class ServerRequesthRoomUser extends Packet {
 
@@ -36,31 +27,6 @@ public class ServerRequesthRoomUser extends Packet {
 
 	@Override
 	public void execPacket() {
-
-		/** 如果有中心 并 目标IP不是中心IP*/
-		if (ConfManager.getIsCenter() && !ConfManager.getCenterIp().equals(this.getRemoteIp())) {
-			/** 转发给中心*/
-			Manager.INSTANCE.sendPacketToCenter(this, Constant.CONSOLE_CODE_TS);
-		} else {
-			String[] roomIds = this.getRoomId().split(",");
-			if (null != roomIds && roomIds.length > 0) {
-				for (String rId : roomIds) {
-					/** 根据房间id获取房间内用户信息*/
-					Map<String, People> hm = ServerDataPool.dataManager.getRoomUesrs(rId);
-
-					String res = JSONObject.toJSONString(hm);
-					/** 实例化获取房间列表 下行 并赋值*/
-					ClientResponehRoomUser crru = new ClientResponehRoomUser(this.getPacketHead());
-					/** json格式的用户信息放入 选项 1 中*/
-					crru.setOption(1,res);
-					crru.setOption(2, this.getOption(2));
-					crru.setOption(3, this.getOption(3));
-					crru.setRoomId(rId);
-					/** 执行*/
-					crru.execPacket();
-				}
-			}
-		}
 
 	}
 
