612d78dff1f90a27eb40c1626fe2eb4c7fbc14f0
user.name <Administrator@DESKTOP-M55IN4K>
2020-02-05 09:16:46 +0800

    update http getroomuser

--- src/com/sa/service/client/ClientResponehRoomUser.java
+++ src/com/sa/service/client/ClientResponehRoomUser.java
@@ -1,25 +1,16 @@
 package com.sa.service.client;
 
-import com.sa.base.ServerDataPool;
 import com.sa.net.Packet;
 import com.sa.net.PacketHeadInfo;
 import com.sa.net.PacketType;
-
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
-import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpHeaders;
-import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.handler.codec.http.HttpVersion;
 
 public class ClientResponehRoomUser extends Packet {
 
 	/**
 	 *
 	 */
-	public ClientResponehRoomUser() {}
+	public ClientResponehRoomUser() {
+	}
 
 	/**
 	 * @param transactionId
@@ -34,7 +25,6 @@ public class ClientResponehRoomUser extends Packet {
 
 	@Override
 	public void execPacket() {
-		sendrs();
 	}
 
 	@Override
@@ -42,24 +32,4 @@ public class ClientResponehRoomUser extends Packet {
 		return PacketType.ClientResponehRoomUser;
 	}
 
-	private void sendrs() {
-		String res = (String) this.getOption(1);
-		HttpRequest request = ServerDataPool.USER_REQUEST_MAP.get(this.getFromUserId());
-		ChannelHandlerContext ctx = ServerDataPool.USER_CHANNEL_MAP_BACK.get(this.getFromUserId());
-		try {
-			FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer(res.getBytes("UTF-8")));
-	        response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain");
-	        response.headers().set(HttpHeaders.Names.CONTENT_LENGTH,
-	                response.content().readableBytes());
-	        if (HttpHeaders.isKeepAlive(request)) {
-	            response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
-	        }
-	        if(null!=ctx){
-	        	ctx.write(response);
-		        ctx.flush();
-	        }
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
