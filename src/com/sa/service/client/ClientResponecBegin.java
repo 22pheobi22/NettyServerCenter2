b366e1c5456ee76c505d76b235b1c6e9b2d8faa5
liyahui <15738773643@163.com>
2020-02-07 09:04:03 +0800

    update 开课

--- src/com/sa/service/client/ClientResponecBegin.java
+++ src/com/sa/service/client/ClientResponecBegin.java
@@ -58,18 +58,19 @@ public class ClientResponecBegin extends Packet {
 		try {
 			/** 遍历用户信息 */
 			for (Entry<String, People> entry : hm.entrySet()) {
+				ClientResponecBegin clientResponecBegin = new ClientResponecBegin(this.getPacketHead());
 				/** 设置目标用户id*/
-				this.setToUserId(entry.getKey());
+				clientResponecBegin.setToUserId(entry.getKey());
 				/** 如果该用户有说话的权利*/
 				if (0 == entry.getValue().getAuth().get(Constant.AUTH_SPEAK)) {
 					/** 设置 消息 为 禁言*/
-					this.setOption(1, Constant.ERR_CODE_10095);
+					clientResponecBegin.setOption(1, Constant.ERR_CODE_10095);
 				} else {
 					/** 设置 消息 为 解除禁言*/
-					this.setOption(1, Constant.ERR_CODE_10096);
+					clientResponecBegin.setOption(1, Constant.ERR_CODE_10096);
 				}
 				/** 发送消息*/
-				Manager.INSTANCE.sendPacketTo(this, Constant.CONSOLE_CODE_S);
+				Manager.INSTANCE.sendPacketTo(clientResponecBegin, Constant.CONSOLE_CODE_S);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
