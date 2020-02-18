083dd796f98dfe104c4d95227d04ddd66e49e044
user.name <Administrator@DESKTOP-M55IN4K>
2020-02-18 15:14:21 +0800

    update

--- src/com/sa/base/ConfManager.java
+++ src/com/sa/base/ConfManager.java
@@ -1,3 +1,10 @@
+package com.sa.base;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sa.util.StringUtil;
+
 public class ConfManager {
 	/** 运行配置 */
 	public static Map<String, String> CONF_MAP = new HashMap<>();
