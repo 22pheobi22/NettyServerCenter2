ffdaa956d05bef6e4b558ae4a74f7b46a3938ac5
zyh <zyh@DESKTOP-VOEM226>
2020-02-25 11:44:04 +0800

    系统管理员-关闭房间 改用client删除数据及向房间内所有人发消息

--- src/com/sa/service/sys/SysCloseRoomReq.java
+++ src/com/sa/service/sys/SysCloseRoomReq.java
@@ -12,9 +12,10 @@
  */
 package com.sa.service.sys;
 
-import com.sa.base.ServerDataPool;
 import com.sa.net.Packet;
 import com.sa.net.PacketType;
+import com.sa.service.client.ClientResponecRoomRemove;
+import com.sa.util.Constant;
 
 public class SysCloseRoomReq extends Packet {
 
@@ -23,13 +24,25 @@ public class SysCloseRoomReq extends Packet {
 
 	@Override
 	public void execPacket() {
-		String roomId = (String) this.getOption(1);
-		/** 删除 房间 消息 缓存 */
+		/*String roomId = (String) this.getOption(1);
+		*//** 删除 房间 消息 缓存 *//*
 		ServerDataPool.dataManager.cleanLogs(roomId);
-		/** 删除 房间 缓存 */
+		*//** 删除 房间 缓存 *//*
 		ServerDataPool.dataManager.removeRoom(roomId);
-		/** 删除 房间 空闲 计数 缓存 */
-		ServerDataPool.dataManager.cancelFreeRoom(roomId);
+		*//** 删除 房间 空闲 计数 缓存 *//*
+		ServerDataPool.dataManager.cancelFreeRoom(roomId);*/
+		
+		String[] roomIds = this.getRoomId().split(",");
+		if (null != roomIds && roomIds.length > 0) {
+			for (String rId : roomIds) {
+				/** 实例化 删除房间 下行*/
+				ClientResponecRoomRemove clientResponecRoomRemove = new ClientResponecRoomRemove(this.getPacketHead());
+				clientResponecRoomRemove.setStatus(20046);
+				clientResponecRoomRemove.setOption(1, Constant.PROMPT_CODE_20046);
+				clientResponecRoomRemove.setRoomId(rId);
+				clientResponecRoomRemove.execPacket();
+			}
+		}
 
 		SysCloseRoomRes sysCloseRoomRes = new SysCloseRoomRes();
 		sysCloseRoomRes.setPacketHead(this.getPacketHead());
